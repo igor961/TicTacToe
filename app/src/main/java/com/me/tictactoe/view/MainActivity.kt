7711e45595ac1f205156894597cf248ca26ffd88
Igor <igor.shesterikov2017@gmail.com>
2020-07-02 23:23:00 +0300

    optimize imports

--- app/src/main/java/com/me/tictactoe/view/MainActivity.kt
+++ app/src/main/java/com/me/tictactoe/view/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.me.tictactoe.view
 
 import android.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
+import androidx.appcompat.app.AppCompatActivity
 import com.me.tictactoe.controller.Game
 import kotlin.concurrent.thread
 
